f0a7293fd7d61a8aa99132d15d90868e1bfce81b
Jan Kowalleck <jan.kowalleck@gmail.com>
2019-10-28 17:08:37 +0100

    replaced `putFile` function with working onw. total overengineered but it saves trafic

--- themes/ease/static/js/put.js
+++ themes/ease/static/js/put.js
@@ -1,15 +1,63 @@
-function putFile(id, file) {
-  var xhttp;
-  let element = document.getElementById(id);
-    if (file) {
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4) {
-          if (this.status == 200) {element.innerHTML = this.responseText;}
-          if (this.status == 404) {element.innerHTML = "Page not found.";}
+
+/**
+ *
+ * @param {string}    id
+ * @param {string}    file
+ * @param {string}    location
+ * @param {function}  onLocation
+ */
+function putFile(id, file, location, onLocation)
+{
+  var targetElem = document.getElementById(id);
+  var targetFallbackHTML = "<p>Page not found.</p>";
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function()
+  {
+    if (this.readyState == 4)
+    {
+      if (this.status == 200)
+      {
+        try
+        {
+          var sourceDoc = (new DOMParser()).parseFromString(this.responseText, 'text/html');
+          var sourceElem = sourceDoc.getElementById(id);
+          replace_onClick(sourceElem, location, onLocation);
+          targetElem.parentElement.replaceChild(sourceElem, targetElem);
+          if ( window.history && window.history.replaceState )
+          {
+             window.history.replaceState({}, sourceDoc.title ,file);
+          }
+          return;
+        }
+        catch (e)
+        {
+          console.error(e);
         }
       }
-      xhttp.open("GET", file, true);
-      xhttp.send();
+      targetElem.innerHTML = targetFallbackHTML;
     }
+  };
+  xhttp.open("GET", file, true);
+  xhttp.send();
+}
+
+/**
+ *
+ * @param {HTMLElement} elem
+ * @param {string}    location
+ * @param {function}  onLocation
+ */
+function replace_onClick (elem, location, onLocation)
+{
+  var onClick = function() { onLocation(); return false; };
+  var as = elem.getElementsByTagName('a');
+  for ( var ai=0, al=as.length, ae ; ai < al ; ++ai )
+  {
+    /** @type {HTMLLinkElement} ae */
+    ae = as[ai];
+    if ( ae.href == location )
+    {
+      ae.onclick = onClick;
+    }
+  }
 }
